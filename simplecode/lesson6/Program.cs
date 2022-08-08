0d858accfc97a6cf83486284653534baa32a4daf
valentin Glass <valentinglass@mail.ru>
2022-08-08 20:25:30 +0300

    add new lesson

--- simplecode/lesson6/Program.cs
+++ simplecode/lesson6/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// C# КОНВЕРТАЦИЯ СТРОКИ В ЧИСЛО | C# КЛАСС CONVERT | C# ОТ НОВИЧКА К ПРОФЕССИОНАЛУ | Урок # 6
+/*string str = "3";
+string str2 = "5";
+
+int a = Convert.ToInt32(str);
+int b = Convert.ToInt32(str2);
+
+System.Console.WriteLine(a + b);*
+*/
+System.Console.WriteLine("Программа складыает 2 числа. Введите первое число-> ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+
+System.Console.WriteLine("Введите второе число-> ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+
+int result = number1 + number2;
+
+System.Console.WriteLine("Сумма двух чисел ровна: " + result);
---
+++ simplecode/lesson7/Program.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Globalization;
+string str = "5.3";
+NumberFormatInfo numberFormatInfo = new NumberFormatInfo()
+{
+    NumberDecimalSeparator = ".",
+};
+
+double a = double.Parse(str, numberFormatInfo);
+System.Console.WriteLine(a);
