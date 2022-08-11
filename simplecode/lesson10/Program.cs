5f3ab941327e3efcb318102088c506861859fe62
valentin Glass <valentinglass@mail.ru>
2022-08-11 20:47:58 +0300

    add lesson10

---
+++ simplecode/lesson10/Program.cs
@@ -0,0 +1,13 @@
+﻿// конструкция логического выбора if else
+
+bool isInfected = false;
+
+if (isInfected)
+{
+    System.Console.WriteLine("Персонаж инфицирован");
+
+}
+else
+{
+    System.Console.WriteLine("Персонаж здоров");
+}
---
+++ simplecode/lesson9/Program.cs
@@ -0,0 +1,9 @@
+﻿// инкремент и дикремент
+
+int a = 0;
+a++;                                 //инкремент  (a = a + 1)
+System.Console.WriteLine(a);
+
+int b = 0;
+b--;                                //дикремент   (b = b -1)
+System.Console.WriteLine(b);
